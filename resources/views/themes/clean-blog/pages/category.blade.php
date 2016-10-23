0408d5de5aff2379d0c3102f8c66956b08931a10
yubin <qybbqybb@gmail.com>
2016-10-22 18:34:52 -0700

    yubin:update view

--- resources/views/themes/clean-blog/pages/category.blade.php
+++ resources/views/themes/clean-blog/pages/category.blade.php
@@ -8,9 +8,9 @@
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Vuedo</h1>
+                        <h1>Toogr8</h1>
                         <hr class="small">
-                        <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                        <span class="subheading">A collection of interesting things</span>
                     </div>
                 </div>
             </div>
