6d9f5d3ef3f12c98aa50e7f937b384e3a795449b
yubin <qybbqybb@gmail.com>
2016-11-22 17:13:34 -0800

    test

---
+++ resources/views/ads.blade.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="row">
+
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+
+          <!--ad 728x90 - DO NOT MODIFY-->
+          <script type="text/javascript">
+            ( function() {
+              if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+              var unit = {"calltype":"async[2]","publisher":"qybbqybb","width":728,"height":90,"sid":"Chitika Default"};
+              var placement_id = window.CHITIKA.units.length;
+              window.CHITIKA.units.push(unit);
+              document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+          }());
+          </script>
+          <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+          <!--ad BANNER 728x90 - DO NOT MODIFY-->
+        </div>
+    </div>
+</div>
