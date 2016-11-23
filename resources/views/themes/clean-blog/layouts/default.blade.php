32d83610c2bd00417af1e5d460151dc672be58fe
yubin <qybbqybb@gmail.com>
2016-11-22 17:20:16 -0800

    yubin:update

--- resources/views/themes/clean-blog/layouts/default.blade.php
+++ resources/views/themes/clean-blog/layouts/default.blade.php
@@ -48,7 +48,14 @@
 
 <!-- Page Header -->
 @yield("header")
-
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+      @include('ads')
+    </div>
+  </div>
+</div>
+@include('ads')
 <!-- Main Content -->
 <div class="container">
     <div class="row">
